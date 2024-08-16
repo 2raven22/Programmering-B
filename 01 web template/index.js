2bc3c1a53e79e6d79270c5b4877c6fd8e358c76f
2raven22 <ravenn1ri@gmail.com>
2024-08-15 23:26:51 -1000

    first commit

---
+++ 01 web template/index.js
@@ -0,0 +1,32 @@
+let currentPage = 1
+//array
+let pages = []
+
+function setup(){
+    console.log('P5.js er loaded')
+
+    pages=selectAll('.page')
+    // kan ses pages er blevet til en liste med alle page class = page ting 
+    // console.log(pages.length)
+}
+
+function shiftPage(num){
+    if(num == "ArrowLeft"){
+        num = currentPage - 1
+    }
+    if(num == "ArrowRight"){
+        num = currentPage + 1
+    }
+
+    if(isNaN(num) || num > pages.length || num == 0){
+        return
+    }
+    select("#page" + currentPage).removeClass('visible')
+    currentPage = num
+    select("#page" + currentPage).addClass('visible')
+}
+
+function keyPressed(){
+    console.log(key)
+    shiftPage(key)
+}
