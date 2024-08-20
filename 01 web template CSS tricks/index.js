7a5cf392385803435c692ae33b72f2ffc9e194ff
2raven22 <ravenn1ri@gmail.com>
2024-08-19 21:28:05 -1000

    Created index.html root

--- 01 web template CSS tricks/index.js
+++ 01 web template CSS tricks/index.js
@@ -1,6 +1,7 @@
-let currentPage = 2
+let currentPage = 3
 //array
 let pages = []
+let colors = ['red', 'green', 'blue', 'orange', 'cyan','purple','babypink']
 
 function setup(){
     console.log('P5.js er loaded')
@@ -8,6 +9,14 @@ function setup(){
     pages=selectAll('.page')
     // kan ses pages er blevet til en liste med alle page class = page ting 
     // console.log(pages.length)
+
+    //laver en masse div til page 3
+    for(c of colors){
+        //console.log(c)
+        let div = createDiv()
+        div.style('background-color', c)
+        select('#page3').child(div)
+    }
 }
 
 function shiftPage(num){
