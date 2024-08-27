a3224e9931a9ffc2dd9cf0290c4c723e9c27d0aa
2raven22 <ravenn1ri@gmail.com>
2024-08-26 21:16:19 -1000

    made moving menu and skeleton for page 1

---
+++ 03_web_app_opg/index.js
@@ -0,0 +1,49 @@
+let currentPage = 1
+
+//array
+let pages = []
+let menuitems //array med menupunkter
+
+function setup(){
+
+
+    pages=selectAll('.page')
+    menuitems=selectAll('.menuitem')
+
+    //løber menu items igennem. Skifter side
+    for(m of menuitems){
+        m.mousePressed(function(e){
+            //e.target er html div
+            console.log(e.target.id)
+            //slice -1 henter det sidste bogstav i en string
+            let nr = e.target.id.slice(-1)
+            shiftPage(nr)
+        })
+    }
+    
+        //shiftPage er funktion der tager tal og skifter til side
+        shiftPage(currentPage)
+}
+
+function shiftPage(num){
+    if(num == "ArrowLeft"){
+        num = currentPage - 1
+    }
+    if(num == "ArrowRight"){
+        num = currentPage + 1
+    }
+
+    if(isNaN(num) || num > pages.length || num == 0){
+        return
+    }
+    select("#page" + currentPage).removeClass('visible')
+    select("#menu" + currentPage).removeClass('active')
+    currentPage = num
+    select("#page" + currentPage).addClass('visible')
+    select("#menu" + currentPage).addClass('active')
+}
+
+function keyPressed(){
+    console.log(key)
+    shiftPage(key)
+}
